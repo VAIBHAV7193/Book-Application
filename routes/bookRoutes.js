032d760c4cf9485850dbb7be466284da0b58e65c
Vaibhav T <vaibhavtawde7193@gmail.com>
2023-11-24 18:25:28 +0530

    new commit

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -21,4 +21,21 @@ router.route('/getallbook').get(getAllBook);
 
 router.route('/getsinglebook/:id').get(getSingleBook);
 
+router.route('/createbook').post(createbook);
+
+//updateBook Route
+
+router.route('/updatebook/:id').put(updateBook)
+
+//delete book
+
+router.route('/deletebook/:id').delete(deleteBook)
+
+//get all book list
+router.route('/getallbook').get(getAllBook);
+
+//get single book list
+
+router.route('/getsinglebook/:id').get(getSingleBook);
+
 module.exports = router;
